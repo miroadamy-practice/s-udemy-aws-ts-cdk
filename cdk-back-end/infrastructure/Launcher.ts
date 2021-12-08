c73765c2d297c1ff11dec320a3f647bb226b10f6
Miro Adamy <miro.adamy@pivotree.com>
2021-12-08 18:57:45 +0100

    Initial code in infra

---
+++ cdk-back-end/infrastructure/Launcher.ts
@@ -0,0 +1,8 @@
+import { SpaceStack, SpaceStack, SpaceStack } from "./SpaceStacks";
+
+import { App } from "aws-cdk-lib";
+
+const app = new App();
+new SpaceStack(app, "Space-Finder-Backend", {
+  stackName: "SpaceFinder",
+});
